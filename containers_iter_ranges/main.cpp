28ccedca96194cdddfa505024f5eb3a2a84d2cc0
stayingcozy <cody.colclough@gmail.com>
2025-11-15 12:14:50 -0600

    finished chp 2, in windows zed environ

--- containers_iter_ranges/main.cpp
+++ containers_iter_ranges/main.cpp
@@ -1,66 +1,85 @@
+#include <climits>
+#include <cstdint>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
 #include <ranges>
-// #include <format>
+#include <format>
 #include <cassert>
 #include <numeric>
 
-// g++ -Wall --std=c++2a -o vect main.cpp && ./vect -W warning flag, -all for all -> all warnings 
+// g++ -Wall --std=c++2a -o vect main.cpp && ./vect -W warning flag, -all for all -> all warnings
+// clang++ -std=c++23 main.cpp -o main.exe && main.exe
+
+bool is_palindrome(const std::vector<int>& v)
+{
+    auto forward = v | std::views::take(v.size() / 2);
+    auto backward = v | std::views::reverse
+                      | std::views::take(v.size() / 2);
+    return std::ranges::equal(forward, backward);
+}
 
 void check_properties(const std::vector<std::vector<int>> & triangle)
 {
   size_t row_number = 1;
-  int expected_total = 1;  
+  int expected_total = 1;
   for (const auto & row : triangle)
   {
     assert(row.front() == 1);
     assert(row.back() == 1);
     assert(row.size() == row_number++);
 
-    // sume of each row -> pow 2 
+    // sume of each row -> pow 2
     assert(std::accumulate(row.begin(), row.end(), 0) == expected_total);
     expected_total *= 2;
+
+    assert(std::ranges::all_of(row, [] (auto x) { return x>=0; }));
+
+    auto negative = [] (int x) { return x<0; };
+    auto negatives = row | std::views::filter(negative);
+    assert(negatives.empty());
+
+    assert(is_palindrome(row));
   }
 }
 
 template<typename T>
-std::ostream& operator << (std::ostream & s, 
-    const std::vector<std::vector<T>>& triangle) 
+std::ostream& operator << (std::ostream & s,
+    const std::vector<std::vector<T>>& triangle)
 {
-  for (const auto& row : triangle) // const, ref to avoid copy 
+  for (const auto& row : triangle) // const, ref to avoid copy
   {
     std::ranges::copy(row, std::ostream_iterator<T>(s, " "));
-    s << '\n'; 
+    s << '\n';
   }
   return s;
 }
 
-// void show_vectors(std::ostream& s,
-//     const std::vector<std::vector<int>>& v)
-// {
-//   size_t final_row_size = v.back().size();
-//   std::string spaces(final_row_size * 3, ' ');
-//   for (const auto& row : v)
-//   {
-//     s << spaces;
-//     if (spaces.size() > 3)
-//       spaces.resize(spaces.size()-3);
-//     for (const auto& data : row)
-//     {
-//       s << std::format("{: ^{}}", data, 6);
-//     }
-//     s << '\n';
-//   }
-// }
+void show_vectors(std::ostream& s,
+   const std::vector<std::vector<int>>& v)
+{
+  size_t final_row_size = v.back().size();
+  std::string spaces(final_row_size * 3, ' ');
+  for (const auto& row : v)
+  {
+    s << spaces;
+    if (spaces.size() > 3)
+      spaces.resize(spaces.size()-3);
+    for (const auto& data : row)
+    {
+      s << std::format("{: ^{}}", data, 6);
+    }
+    s << '\n';
+  }
+}
 
 std::vector<int> get_next_row(const std::vector<int>& last_row)
 {
   std::vector next_row { 1 }; // CTAD deduce type by 1 (int) Class template argument detection
   if (last_row.empty())
   {
-    return next_row; 
+    return next_row;
   }
   for (size_t idx = 0; idx+1 < last_row.size(); ++idx)
   {
@@ -74,19 +93,37 @@ std::vector<int> get_next_row(const std::vector<int>& last_row)
 auto generate_triangle(int rows)
 {
   std::vector<std::vector<int>> triangle{ {1} };
-  for (int row=0; row < rows; ++row) 
+  for (int row=0; row < rows; ++row)
   {
     triangle.push_back(get_next_row(triangle.back()));
   }
-  
+
   return triangle;
 }
 
-int main() 
+void show_view(std::ostream& s, const std::vector<std::vector<int>>& v)
 {
-  
-  auto triangle = generate_triangle(16); 
-  std::cout << triangle;
+    std::string spaces(v.back().size(), ' ');
+    for (const auto& row : v)
+    {
+        s << spaces;
+        if (spaces.size())
+            spaces.resize(spaces.size() - 1);
+        auto odds = row | std::views::transform( [] (int x) { return x % 2 ? '*' : ' '; });
+        for (const auto& data : odds)
+        {
+            s << data << ' ';
+        }
+        s << '\n';
+    }
+}
+
+int main()
+{
+
+  auto triangle = generate_triangle(16);
+  //std::cout << triangle;
   check_properties(triangle);
-  // show_vectors(std::cout, triangle);
+  show_vectors(std::cout, triangle);
+  show_view(std::cout, triangle);
 }
