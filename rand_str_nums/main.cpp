6e3e697b722ac495a5d4c5eaa1788899ec495aad
stayingcozy <cody.colclough@gmail.com>
2025-11-16 19:29:25 -0600

    final form, rand's and vector of lambdas

--- rand_str_nums/main.cpp
+++ rand_str_nums/main.cpp
@@ -1,4 +1,5 @@
-#include <concepts>
+#include <cassert>
+// #include <concepts>
 #include <ios>
 #include <iostream>
 #include <limits>
@@ -7,9 +8,41 @@
 #include <format>
 #include <random>
 
+constexpr bool is_prime(int n)
+{
+    if (n == 2 | n == 3)
+        return true;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; ++i)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+
+
 unsigned return_const_number()
 {
     return 42;
+}
+
+int some_prime_number()
+{
+    std::random_device rd;
+    std::mt19937 engine { rd() };
+    std::uniform_int_distribution<int> dist(1, 9999);
+    int n = {};
+    while (!is_prime(n))
+    {
+        n = dist( engine );
+    }
+    return n;
 }
 
 int some_random_number()
@@ -45,8 +78,37 @@ std::optional<int> read_number(std::istream& in)
     return {};
 }
 
-void guess_number_with_clues(int number, std::invocable<int, int> auto message) //(used) less strict but still specific for compiler OR strict typing -> std::function<std::string(int, int)> message)
+std::string check_which_digits_correct(int number, int guess)
 {
+    auto ns = std::format("{:0>5}",(number));
+    auto gs = std::format("{:0>5}", (guess));
+    std::string matches(5, '.');
+    for (size_t i=0, stop=gs.length(); i < stop; ++i)
+    {
+        char guess_char = gs[i];
+        if (i < ns.length() && guess_char == ns[i])
+        {
+            matches[i] = '*';
+            ns[i] = '*';
+        }
+    }
+    for (size_t i=0, stop=gs.length(); i<stop; ++i)
+    {
+        char guess_char = gs[i];
+        if (i < ns.length() && matches[i] != '*')
+        {
+            if (size_t idx = ns.find(guess_char, 0); idx != std::string::npos)
+            {
+                matches[i] = '^';
+                ns[idx] = '^';
+            }
+        }
+    }
+    return matches;
+}
+
+void guess_number_with_clues(int number, auto messages) //(used) less strict but still specific for compiler OR strict typing -> std::function<std::string(int, int)> message)
+{ // also std::invocable<std::string(int, int) message
     std::cout << "Guess the number. \n";
     std::optional<int> guess;
     while ((guess = read_number(std::cin)))
@@ -56,10 +118,20 @@ void guess_number_with_clues(int number, std::invocable<int, int> auto message) 
             std::cout << "Well Done.";
             return;
         }
-        std::cout << message(number, guess.value());
+        std::cout << std::format("{:0<5} is wrong. Try again\n", guess.value());
+
+        for (auto message : messages)
+        {
+            auto clue = message(guess.value());
+            if (clue.length())
+            {
+                std::cout << clue;
+                break;
+            }
+        }
         std::cout << '>';
     }
-    std::cout << std::format("The number was {}\n", number);
+    std::cout << std::format("The number was {:0>5}\n", number);
 }
 
 void guess_number_or_give_up(int number)
@@ -90,12 +162,45 @@ void guess_number(unsigned number)
     std::cout << "Well Done. \n";
 }
 
+void check_properties()
+{
+    static_assert(is_prime(2));
+    assert(check_which_digits_correct(12347, 23471) == "^^^^^");
+}
+
 int main() {
     // guess_number(return_const_number());
     // guess_number_or_give_up(return_const_number());
 
-    auto make_message = [] (int number, int guess) {
-        return std::format("Your guess was too {}\n", (guess < number ? "small" : "big"));
+    check_properties();
+
+    auto check_prime = [] (int guess) {
+        return std::string((is_prime(guess) ? "" : "Not Prime\n"));
     };
-    guess_number_with_clues(some_random_number(), make_message);
+
+    auto check_length = [] (int guess) {
+        return std::string((guess < 100000) ? "" : "Too long\n");
+    };
+
+    int number = some_prime_number();
+    auto check_digits = [number] (int guess) {
+        return std::format("{}\n",
+            check_which_digits_correct(number, guess));
+    };
+
+    std::vector<std::function<std::string(int)>> messages {
+        check_length,
+        check_prime,
+        check_digits
+    };
+
+    // auto make_message = [] (int number, int guess) {
+    //     return std::format("Your guess was too {}\n", (guess < number ? "small" : "big"));
+    // };
+    // auto make_message = [] (int number, int guess) {
+    //     return std::format("{}\n{}\n",
+    //         is_prime(guess) ? "Prime" : "Not Prime",
+    //         check_which_digits_correct(number, guess));
+    // };
+    guess_number_with_clues(number, messages);
 }
