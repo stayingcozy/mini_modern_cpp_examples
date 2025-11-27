8be807ccb2928698464b6f1b64f29566858acf16
stayingcozy <cody.colclough@gmail.com>
2025-11-27 06:02:59 -0600

    card game made. Now just need to monetize then profit???

--- deck/playingcards.cpp
+++ deck/playingcards.cpp
@@ -1,3 +1,9 @@
+#include <algorithm>
+#include <algorithm>
+#include <ostream>
+#include <random>
+#include <array>
+#include <variant>
 #include "playingcards.hpp"
 
 namespace cards
@@ -41,6 +47,15 @@ namespace cards
         os << to_string(card.value()) << " of " << to_string(card.suit());
         return os;
     }
+    std::ostream& operator<<(std::ostream& os, const std::variant<Card, Joker>& card)
+    {
+        if(std::holds_alternative<Joker>(card))
+            os << "JOKER";
+        else
+            os << std::get<Card>(card);
+        return os;
+    }
+    
     Suit& operator++(Suit& suit)
     {
         using IntType = typename std::underlying_type<Suit>::type; // underlying enum type
@@ -49,5 +64,98 @@ namespace cards
         else
             suit = static_cast<Suit>(static_cast<IntType>(suit) + 1);
         return suit;
+    }
+    
+    void shuffle_deck(std::array<Card, 52>& deck)
+    {
+        std::random_device rnd;
+        std::mt19937 gen{ rnd() };
+        std::ranges::shuffle(deck, gen);
+    }
+    void shuffle_deck(std::array<std::variant<Card, Joker>, 54>& deck)
+    {
+        std::random_device rnd;
+        std::mt19937 gen{ rnd() };
+        std::ranges::shuffle(deck, gen);
+    }
+    
+    std::array<Card, 52> create_deck()
+    {
+        std::array<Card, 52> deck;
+        int value = 1;
+        Suit suit = Suit::Hearts;
+        std::ranges::generate(deck, [&value, &suit]() {
+            if (value > 13)
+            {
+                value = 1;
+                ++suit;
+            }
+            return Card{ FaceValue(value++), suit };
+        });
+        return deck;
+    }
+    std::array<std::variant<Card, Joker>, 54> create_extended_deck()
+    {
+        std::array<std::variant<Card, Joker>, 54> deck{ Joker{}, Joker{} };
+        std::array<Card, 52> cards = create_deck();
+        std::ranges::copy(cards, deck.begin()+2);
+        return deck;
+    }
+    
+    bool is_guess_correct(char guess, const Card& current, const Card& next)
+    {
+        return (guess == 'h' && next > current) || (guess == 'l' && next < current);
+    }
+    bool is_guess_correct(char guess, const std::variant<Card, Joker>& current, const std::variant<Card, Joker>& next)
+    {
+        if (std::holds_alternative<Joker>(current) || std::holds_alternative<Joker>(next))
+            return true;
+        Card current_card = std::get<Card>(current);
+        Card next_card = std::get<Card>(next);
+        return is_guess_correct(guess, current_card, next_card);
+    }
+   
+    void higher_lower()
+    {
+        auto deck = create_deck();
+        shuffle_deck(deck);
+        
+        size_t index = 0;
+        while (index+1 < deck.size())
+        {
+            std::cout << deck[index] << ": Next card higher (h) or lower (l)?\n";
+            char c;
+            std::cin >> c;
+            bool ok = is_guess_correct(c, deck[index], deck[index+1]);
+            if (!ok)
+            {
+                std::cout << "Next card was " << deck[index+1] << '\n';
+                break;
+            }
+            ++index;
+        }
+        std::cout << "You got " << index << " correct\n";
+    }
+    
+    void higher_lower_with_jokers()
+    {
+        auto deck = create_extended_deck();
+        shuffle_deck(deck);
+        
+        size_t index = 0;
+        while (index+1 < deck.size())
+        {
+            std::cout << deck[index] << ": Next card higher (h) or lower (l)?\n";
+            char c;
+            std::cin >> c;
+            bool ok = is_guess_correct(c, deck[index], deck[index+1]);
+            if (!ok)
+            {
+                std::cout << "Next card was " << deck[index+1] << '\n';
+                break;
+            }
+            ++index;
+        }
+        std::cout << "You got " << index << " correct\n";
     }
 }
--- deck/playingcards.hpp
+++ deck/playingcards.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <variant>
 
 namespace cards
 {
@@ -10,6 +11,10 @@ namespace cards
         Diamonds,
         Clubs,
         Spades
+    };
+    
+    struct Joker
+    {
     };
 
     class FaceValue
@@ -27,6 +32,7 @@ namespace cards
          int value() const{
              return value_;
          }
+         auto operator<=>(const FaceValue&) const = default;
      private:
          int value_;
     };
@@ -38,15 +44,29 @@ namespace cards
             Card(FaceValue value, Suit suit) : value_(value), suit_(suit) {}
             FaceValue value() const { return value_; }
             Suit suit() const { return suit_; }
-
+            auto operator<=>(const Card&) const = default;
         private:
             FaceValue value_{1};
             Suit suit_{};
     };
 
     std::ostream& operator<<(std::ostream& os, const Card& card);
+    std::ostream& operator<<(std::ostream& os, const std::variant<Card, Joker>& card);
+    
     std::string to_string(const Suit& suit);
     std::string to_string(const FaceValue& value);  
     
     Suit& operator++(Suit& suit);
+    
+    void shuffle_deck(std::array<Card, 52>& deck);
+    void shuffle_deck(std::array<std::variant<Card, Joker>, 54>& deck);
+    
+    bool is_guess_correct(char guess, const Card& current, const Card& next);
+    bool is_guess_correct(char guess, const std::variant<Card, Joker>& current, const std::variant<Card, Joker>& next);
+       
+    std::array<Card, 52> create_deck();
+    std::array<std::variant<Card, Joker>, 54> create_extended_deck();
+    
+    void higher_lower();
+    void higher_lower_with_jokers();
 }
