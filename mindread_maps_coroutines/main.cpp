33ca1b07d11d85ab215fe025d4ced024fe846fd0
stayingcozy <cody.colclough@gmail.com>
2025-12-21 14:09:14 -0600

    coroutine added

--- mindread_maps_coroutines/main.cpp
+++ mindread_maps_coroutines/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <optional>
-#include <string>
 #include <random>
 #include <cassert>
+#include <optional>
+#include <string>
+#include <iostream>
+#include <coroutine>
+#include <memory>
 
 #include "MindReader.hpp"
 
@@ -29,6 +32,108 @@ std::optional<int> read_number(std::istream& in)
         return 1;
     }
     return {};
+}
+
+template<typename Promise>
+struct coro_deleter
+{
+    void operator() (Promise* promise) const noexcept
+    {
+        auto handle = std::coroutine_handle<Promise>::from_promise(*promise);
+        if (handle)
+            handle.destroy();
+    }
+};
+//
+template<typename T>
+using promise_ptr = std::unique_ptr<T, coro_deleter<T>>;
+//
+struct Task{
+    struct promise_type
+    {
+        std::pair<int, int> choice_and_prediction;
+        Task get_return_object()
+        {
+            return Task(this);
+        }
+        std::suspend_never initial_suspend() noexcept
+        {
+            return {};
+        }
+        std::suspend_always final_suspend() noexcept
+        {
+            return {};
+        }
+        void unhandled_exception() {}
+        std::suspend_always yield_value(std::pair<int, int> got)
+        {
+            choice_and_prediction = got;
+            return {};
+        }
+        void return_void() {}
+    };
+    
+    std::pair<int, int> choice_and_prediction()
+    {
+        return promise->choice_and_prediction;
+    }
+    
+    bool done() const
+    {
+        auto handle = std::coroutine_handle<promise_type>::from_promise(*promise);
+        return handle.done();
+    }
+    void next()
+    {
+        auto handle = std::coroutine_handle<promise_type>::from_promise(*promise);
+        return handle ();
+    }
+private:
+    promise_ptr<promise_type> promise;
+    Task(promise_type* p) : promise(p) {}
+};
+Task coroutine_game()
+{
+    std::mt19937 gen{ std::random_device{}() };
+    std::uniform_int_distribution dist{ 0,1 };
+    MindReader mr(gen, dist);
+    while(true)
+    {
+        auto input = read_number(std::cin);
+        if (!input)
+        {
+            co_return;
+        }
+        int player_choice = input.value();
+        co_yield{ player_choice, mr.get_prediction() };
+        mr.update(player_choice);
+    }
+}
+
+void coroutine_mind_reader()
+{
+    int turns = 0;
+    int player_wins = 0;
+    
+    std::cout << "Select 0 or 1 at random and press enter.\n";
+    std::cout << "If the computer predicts your guess it wins.\n"
+        "and it can now read your mind.\n";
+    
+    Task game = coroutine_game();
+    
+    while (!game.done())
+    {
+        auto [player_choice, prediction] = game.choice_and_prediction();
+        ++turns;
+        std::cout << "You pressed " << player_choice << ", I guessed " << prediction << '\n';
+        
+        if (player_choice != prediction)
+        {
+            ++player_wins;
+        }
+        game.next();
+    }
+    std::cout << "you win " << player_wins << '\n' << "machine won " << (turns - player_wins) << '\n';
 }
 
 void pennies_game()
@@ -105,7 +210,8 @@ void mind_reader()
 int main()
 {
     // pennies_game();
-    mind_reader();
+    // mind_reader();
+    coroutine_mind_reader();
     
     return 0;
 }
